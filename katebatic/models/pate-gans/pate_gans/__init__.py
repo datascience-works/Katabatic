c14eae0e900242a21a72989c712dfa0524d38d02
tomadonna1 <tomdeptrai1@gmail.com>
2025-09-15 03:48:51 +1000

    fix utils function

--- katebatic/models/pate-gans/pate_gans/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from .original.original_api import PG_ORIGINAL
-except ImportError:
-    from original.original_api import PG_ORIGINAL
-
-# For now, just export the original
-__all__ = ['PG_ORIGINAL']
--- katebatic/models/pate-gans/pate_gans/turing/__init__.py
+++
@@ -1,3 +0,0 @@
-from pate_gans.turing.turing import PG_TURING
-from pate_gans.turing.turing_hyp import PG_TURING_HYP
-from pate_gans.turing.turing_audit import PG_TURING_AUDIT
