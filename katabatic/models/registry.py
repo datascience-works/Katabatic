cb9d28313144884dff52baa6517d373d38364898
michaelmorks <s224437209@deakin.edu.au>
2026-09-15 13:24:29 +0800

    feat: add ARF synthetic data model and benchmarks (#100)
    Co-authored-by: Michael Morks <michaelmokrs30@gmail.com>
    Co-authored-by: Zachariah George Christie <zachariah.christie05@gmail.com>

--- katabatic/models/registry.py
+++ katabatic/models/registry.py
@@ -82,6 +82,13 @@ class ModelRegistry:
             "extra": "ctgan",
             "supported": True,
         },
+        "arf": {
+            "module": "katabatic.models.arf.models",
+            "class": "ARFModel",
+            "dependencies": ["sklearn", "numpy", "pandas"],
+            "extra": None,
+            "supported": False,
+        },
     }
 
     @classmethod
